2be6855d74c01be5eb0981277f1e72acf5011733
Tadukoo <realtadukoo@gmail.com>
2018-03-17 13:12:00 -0400

    Added Another Test to MapTileTest.

--- junit/edu/ycp/cs320/middle_earth/model/Constructs/MapTileTest.java
+++ junit/edu/ycp/cs320/middle_earth/model/Constructs/MapTileTest.java
@@ -23,4 +23,26 @@ public class MapTileTest{
 		//assertEquals(10, tile.getConnections().get("north"));
 		throw new UnsupportedOperationException("Waiting on Chris's fix");
 	}
+	
+	@Test
+	public void testResetConnections(){
+		HashMap<String, Integer> conns = new HashMap<String, Integer>();
+		conns.put("north", 10);
+		
+		// TODO: Wait for Chris's correction of MapTile (currently connections = <HashMap<String, MapTile>)
+		//tile.setConnections(conns);
+		//assertEquals(10, tile.getConnections().get("north"));
+		
+		HashMap<String, Integer> conns2 = new HashMap<String, Integer>();
+		conns2.put("south", 25);
+		conns2.put("northeast", 50);
+		
+		// TODO: Wait for Chris's correction of MapTile (currently connections = <HashMap<String, MapTile>)
+		//tile.setConnections(conns2);
+		//assertEquals(null, tile.getConnections().get("north"));
+		//assertEquals(25, tile.getConnections().get("south"));
+		//assertEquals(50, tile.getConnections().get("northeast"));
+		
+		throw new UnsupportedOperationException("Waiting on Chris's fix");
+	}
 }
