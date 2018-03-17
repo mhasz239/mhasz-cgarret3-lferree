d519cf697f88bf5cbbe8ba5aa701e58b9388ad38
Tadukoo <realtadukoo@gmail.com>
2018-03-17 12:34:20 -0400

    Added another test to MapTest

--- junit/edu/ycp/cs320/middle_earth/model/Constructs/MapTest.java
+++ junit/edu/ycp/cs320/middle_earth/model/Constructs/MapTest.java
@@ -21,10 +21,39 @@ public class MapTest{
 		
 		ArrayList<MapTile> mapTiles = new ArrayList<MapTile>();
 		MapTile tile = new MapTile();
+		mapTiles.add(tile);
 		
 		map.setMapTiles(mapTiles);
 		
 		assertEquals(1, map.getMapTiles().size());
 		assertEquals(tile, map.getMapTiles().get(0));
+	}
+	
+	@Test
+	public void testResetMapTiles(){
+		assertEquals(0, map.getMapTiles().size());
+		
+		ArrayList<MapTile> mapTiles = new ArrayList<MapTile>();
+		MapTile tile = new MapTile();
+		mapTiles.add(tile);
+		
+		map.setMapTiles(mapTiles);
+		
+		assertEquals(1, map.getMapTiles().size());
+		assertEquals(tile, map.getMapTiles().get(0));
+		
+		ArrayList<MapTile> mapTiles2 = new ArrayList<MapTile>();
+		MapTile tile1 = new MapTile();
+		tile1.setName("Basic");
+		MapTile tile2 = new MapTile();
+		tile2.setName("Other Basic");
+		mapTiles2.add(tile1);
+		mapTiles2.add(tile2);
+		
+		map.setMapTiles(mapTiles2);
+		
+		assertEquals(2, map.getMapTiles().size());
+		assertEquals(tile1, map.getMapTiles().get(0));
+		assertEquals(tile2, map.getMapTiles().get(1));
 	}
 }
