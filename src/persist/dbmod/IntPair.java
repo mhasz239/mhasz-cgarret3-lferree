8d6fceda2a47eba19a01fcf6ca16535f013af136
Chris Garrety <cgarret3@ycp.edu>
2018-05-07 12:48:12 -0400

    Changed helper class(es) to IntPair

---
+++ src/persist/dbmod/IntPair.java
@@ -0,0 +1,27 @@
+package persist.dbmod;
+
+public class IntPair {
+	
+	private int int1;
+	private int int2;
+	
+	public IntPair() {
+		
+	}
+	
+	public void setInt1(int int1) {
+		this.int1 = int1;
+	}
+	
+	public int getInt1() {
+		return this.int1;
+	}
+	
+	public void setInt2(int int2) {
+		this.int2 = int2;
+	}
+	
+	public int getInt2() {
+		return this.int2;
+	}
+}
